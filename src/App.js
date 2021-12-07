f40c1304b4933ddb6f8b5fcffadb40d8eb4c07cd
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-06 17:29:33 -0800

    Setup SampleFlashcard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
+    const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
   return (
-    <h1>Hello Marissa</h1>
+    <FlashcardList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+    {
+        id: 1,
+        question: 'What is your name?',
+        answer: 'Cual es su nombre?',
+        options: [
+            'Cual es su nombre?',
+            'Mucho gusto',
+            'Buenos Dias',
+            'Quieres comida?'
+        ]
+    },
+    {
+        id: 2,
+        question: 'Where are you from?',
+        answer: 'De donde eres?',
+        options: [
+            'Te conozco?',
+            'Encantado',
+            'De donde eres?',
+            'Quieren comida?'
+        ]
+    },
+    {
+        id: 3,
+        question: 'What would you like to eat?',
+        answer: 'Qué te gustaría comer?',
+        options: [
+            'Qué te gustaría comer?',
+            'Mucho gusto?',
+            'Encantado',
+            'Buenas tardes?'
+        ]
+    }
+]
+
 
 export default App;
