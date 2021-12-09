1d8f66f6b7b7176574d87c6d6c9d64175c89241b
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-09 10:45:52 -0800

    Add styles for generate btn

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     }
 
     function handleSubmit(e) {
-        e.preventDefault()
+        e.preventDefault();
     }
 
     return (
@@ -64,7 +64,7 @@ function App() {
                 </div>
                 <div className="form-group">
                     <label htmlFor="amount">Number of Questions</label>
-                    <input type="number" id="amount" min="1" step="1" defaulValue={10} ref={amountEl} />
+                    <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
                 </div>
                 <div className="form-group">
                     <button className="btn">Generate</button>
